80e25cd4a5e15c446101359c62b6ac3df065f924
Dhairya <dhairya.ostwal@gmail.com>
2021-07-13 20:44:07 +0530

    Added Component HC9

---
+++ src/components/HC9/HC9.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HC9.css';
+
+export default function HC9() {
+  return (
+    <div style={{ overflow: 'none' }}>
+      <div className="HC9">
+        <div className="HC9a"></div>
+        <div className="HC9b"></div>
+        <div className="HC9c"></div>
+      </div>
+    </div>
+  );
+}
