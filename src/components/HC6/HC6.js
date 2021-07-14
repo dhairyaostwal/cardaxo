4705d13a944f30e836c41378382bb9167ec3e9c5
Dhairya <dhairya.ostwal@gmail.com>
2021-07-14 17:11:43 +0530

    Added Component: HC6

--- src/components/HC6/HC6.js
+++ src/components/HC6/HC6.js
@@ -1,32 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './HC6.css';
 import arrow from '../../img/arrow.png';
+import { HC6Data } from '../../data/HC6Data';
 
-export default function HC6() {
-  const [HC6list, setHC6List] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      const baseURL =
-        'https://run.mocky.io/v3/fefcfbeb-5c12-4722-94ad-b8f92caad1ad';
-      const res = await axios.get(baseURL);
-      // console.log(res.data.card_groups[0]);
-      setHC6List(res.data.card_groups[0]);
-    }
-
-    getData();
-  });
-
+function HC6Component(props) {
   return (
-    <div className="wrapper">
-      <div className="HC6">
-        <img className="HC6logo" src={HC6list.cards[0].icon.image_url} />
-        <h3 style={{ fontWeight: '500' }}>{HC6list.cards[0].title}</h3>
-        <img className="fampay--arrow" src={arrow}>
-          {/* <a href={HC6list.cards[0].url} /> */}
-        </img>
-      </div>
+    <div className="HC6">
+      <img className="HC6logo" src={props.icon.image_url} />
+      <h3 style={{ fontWeight: '500' }}><a href={props.url}>{props.title}</a></h3>
+      <img className="fampay--arrow" src={arrow} />
     </div>
   );
+}
+
+// export default function HC6() {
+//   const [HC6list, setHC6List] = useState([]);
+
+//   useEffect(() => {
+//     async function getData() {
+//       const baseURL =
+//         'https://run.mocky.io/v3/fefcfbeb-5c12-4722-94ad-b8f92caad1ad';
+//       const res = await axios.get(baseURL);
+//       // console.log(res.data.card_groups[0]);
+//       setHC6List(res.data.card_groups[0]);
+//     }
+
+//     getData();
+//   });
+
+//   return (
+//     <div className="wrapper">
+//       <div className="HC6">
+//         <img className="HC6logo" src={HC6list.cards[0].icon.image_url} />
+//         <h3 style={{ fontWeight: '500' }}>{HC6list.cards[0].title}</h3>
+//         <img className="fampay--arrow" src={arrow}>
+//           {/* <a href={HC6list.cards[0].url} /> */}
+//         </img>
+//       </div>
+//     </div>
+//   );
+// }
+
+export default function HC6() {
+  return <div>{HC6Data.map(HC6Component)}</div>;
 }
