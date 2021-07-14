8f7091028af6cac8a9f97d4a9742de5332fce34d
Dhairya <dhairya.ostwal@gmail.com>
2021-07-14 16:08:58 +0530

    Data files: HC5 and HC9

---
+++ src/data/HC5Data.js
@@ -0,0 +1,50 @@
+export const HC5Data = [
+  {
+    name: 'Savage Revenge',
+    url: 'https://www.gamezop.com/g/ry6bwfUt_Jg?id=bM14MfArj&fp_type=fpGame',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/a612b3e534ba4ca389293a2b92c7ba25.jpeg',
+      aspect_ratio: 2.1548822,
+    },
+    bg_color: '#050402',
+    is_disabled: false,
+  },
+  {
+    name: 'Tower twist',
+    url: 'https://www.gamezop.com/g/HJT46GkPcy7?id=bM14MfArj&fp_type=fpGame',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/8521293693574a81ad9e1f47769ee820.jpeg',
+      aspect_ratio: 2.1548822,
+    },
+    bg_color: '#2b1157',
+    is_disabled: false,
+  },
+  {
+    name: 'Rocket Man',
+    url: 'https://www.gamezop.com/g/SyXuN7W1F?id=bM14MfArj&fp_type=fpGame',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/d9261c88c6fc4d8e9631fd53fdd1408f.jpeg',
+      aspect_ratio: 2.1548822,
+    },
+    bg_color: '#151515',
+    is_disabled: false,
+  },
+  {
+    name: 'Grumpy gorilla',
+    url: 'https://www.gamezop.com/g/N1sZfO1fWqg?id=bM14MfArj&fp_type=fpGame',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/be744c8bef704487b744d355ff292871.jpeg',
+      aspect_ratio: 2.1548822,
+    },
+    bg_color: '#2e5d62',
+    is_disabled: false,
+  },
+];
---
+++ src/data/HC9Data.js
@@ -0,0 +1,46 @@
+export const HC9Data = [
+  {
+    name: 'Club for teenagers (GenZ & GenS)',
+    url: 'https://youtube.com/',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/718171493a944663a167a71c11c5e10a.png',
+      aspect_ratio: 0.84,
+    },
+    is_disabled: false,
+  },
+  {
+    name: 'Rewards (For all personas)',
+    url: 'https://viserion.fampay.in/rewards/?fp_type=fpSurvey&fp_hide_bar=true&fp_bar_color=1f1f1f&tab=2&fp_ios_webview_type=2',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/8b9c873bc2cf42a39523b589b319990a.png',
+      aspect_ratio: 0.84,
+    },
+    is_disabled: false,
+  },
+  {
+    name: 'Transactions History (For all personas)',
+    url: 'https://google.com/',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/1f388bca015345a7ad125fc40e45d1ae.png',
+      aspect_ratio: 0.84,
+    },
+    is_disabled: false,
+  },
+  {
+    name: 'Store (For all personas)',
+    url: 'https://',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/f8f9a6536f9a4e17aff3d25fbef3c12a.png',
+      aspect_ratio: 0.84,
+    },
+    is_disabled: false,
+  },
+];
