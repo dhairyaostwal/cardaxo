d79145c01e4c3ca06bf157c670595cff41428d9c
Dhairya <dhairya.ostwal@gmail.com>
2021-07-14 17:11:27 +0530

    Added Data: HC6

---
+++ src/data/HC6Data.js
@@ -0,0 +1,24 @@
+export const HC6Data = [
+  {
+    name: 'UPI Register card',
+    title: 'Setup your UPI ID',
+    formatted_title: {
+      text: 'Setup your UPI ID',
+      align: 'left',
+      entities: [],
+    },
+    description: 'Register now and start UPI payments',
+    formatted_description: {
+      text: 'Register now and start UPI payments',
+      align: 'left',
+      entities: [],
+    },
+    icon: {
+      image_type: 'ext',
+      image_url:
+        'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/128x128/plain/shape_square.png',
+    },
+    url: 'https://youtube.com/',
+    is_disabled: false,
+  },
+];
