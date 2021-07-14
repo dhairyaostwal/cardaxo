973f0849491ce9fe33b72f25f7b253b7399628ff
Dhairya <dhairya.ostwal@gmail.com>
2021-07-14 23:15:46 +0530

    HC3: Added Data file

---
+++ src/data/HC3MessageData.js
@@ -0,0 +1,13 @@
+import bell from '../img/bell.png';
+import cross from '../img/cross.png';
+
+export const HC3MessageData = [
+  {
+    message: 'remind later',
+    img_url: bell,
+  },
+  {
+    message: 'dismiss now',
+    img_url: cross,
+  }
+];
