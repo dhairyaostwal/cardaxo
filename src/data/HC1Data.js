ce30b2f9cc4b204602e8e3f28a17992e25194bdd
Dhairya <dhairya.ostwal@gmail.com>
2021-07-14 13:08:22 +0530

    Data Added: HC1

---
+++ src/data/HC1Data.js
@@ -0,0 +1,28 @@
+export const HC1Data = [
+  {
+    name: 'View all txn',
+    title: 'Transactions',
+    icon: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/4ce76db9e755497f8d176764b6d590ba.png',
+      aspect_ratio: 1
+    },
+    url: 'https://google.com/',
+    bg_color: '#FFFFFF',
+    is_disabled: false
+  },
+  {
+    name: 'Rewards',
+    title: 'Rewards',
+    icon: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/aa7354aad52243728066169761045344.png',
+      aspect_ratio: 1
+    },
+    url: 'https://youtube.com/',
+    bg_color:'#FBAF03',
+    is_disabled: false
+  }
+];
---
+++ src/data/HC1DataSmallBox.js
@@ -0,0 +1,32 @@
+import Bitmap from './../img/Bitmap.png'
+import userIcon from './../img/userIcon.png'
+
+export const HC1DataSmallBox = [
+    {
+      name: 'SmallCard',
+      title: 'SmallCard',
+      icon: {
+        image_type: 'ext',
+        image_url:
+          userIcon,
+        aspect_ratio: 1
+      },
+      url: 'https://google.com/',
+      bg_color: '#FFFFFF',
+      is_disabled: false
+    },
+    {
+      name: 'SmallCard',
+      title: 'AryaStark',
+      icon: {
+        image_type: 'ext',
+        image_url:
+          Bitmap,
+        aspect_ratio: 1
+      },
+      url: 'https://youtube.com/',
+      bg_color:'#FBAF03',
+      is_disabled: false
+    }
+  ];
+  
