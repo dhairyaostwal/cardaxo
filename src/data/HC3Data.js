e558e696d784d759e5ba3c0640eaaf7d6fa829d8
Dhairya <dhairya.ostwal@gmail.com>
2021-07-14 18:33:54 +0530

    Data Added: HC3

---
+++ src/data/HC3Data.js
@@ -0,0 +1,33 @@
+export const HC3Data = [
+  {
+    name: 'Add money',
+    title: 'Your account looks empty!',
+    formatted_title: {
+      text: 'Your account looks empty!',
+      entities: [],
+    },
+    description: 'Add some money to your account to get started.',
+    formatted_description: {
+      text: 'Add some money to your account to get started.',
+      entities: [],
+    },
+    url: 'https://facebook.com/',
+    bg_image: {
+      image_type: 'ext',
+      image_url:
+        'https://westeros-staging.s3.amazonaws.com/media/images/generic/5e97239d1bd747878828852d4f397361.png',
+      aspect_ratio: 0.9142857,
+    },
+    bg_color: '#FFB486',
+    cta: [
+      {
+        text: 'Add',
+        bg_color: '#000000',
+        text_color: '#FFFFFF',
+        url_choice: 'add',
+        url: 'https://instagram.com/',
+      },
+    ],
+    is_disabled: false,
+  },
+];
