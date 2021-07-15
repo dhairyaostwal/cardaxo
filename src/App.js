f59ee7921c22e440c45fe6e3bf05b56dfd79d2ec
Dhairya <dhairya.ostwal@gmail.com>
2021-07-15 07:22:10 +0530

    Added: Page Pull on Reload Functionality

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import HC5 from './components/HC5/HC5';
 import HC6 from './components/HC6/HC6';
 import HC3 from './components/HC3/HC3';
 import HC1 from './components/HC1/HC1';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+
+// function which on pull will manipulates DOM for reloading page
+function handleRefresh() {
+  document.location.reload();
+}
 
 export function App() {
   return (
     <>
-      <Navbar />
-      <HC3 />
-      <HC6 />
-      <HC5 />
-      <HC9 />
-      <HC1 />
+      <PullToRefresh onRefresh={handleRefresh}>
+        <Navbar />
+        <HC3 />
+        <HC6 />
+        <HC5 />
+        <HC9 />
+        <HC1 />
+      </PullToRefresh>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 import './index.css';
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
